63b12d9222274506fa488636c63a5e857fdc30e7
abdealijaroli <abdealiburu03@gmail.com>
2021-07-30 08:46:33 +0530

    joi convert set to false

--- middleware/joiSchemaValidation.js
+++ middleware/joiSchemaValidation.js
@@ -3,7 +3,6 @@ const productSchema = require('../apiSchema/productSchema')
 const validateObjectSchema = (data) => {
     const result = productSchema.createProductSchema.validate(data, { convert: false });
 }
-
 module.exports.validateBody = () => {
     return (req, res, next) => {
         validateObjectSchema(req.body);
